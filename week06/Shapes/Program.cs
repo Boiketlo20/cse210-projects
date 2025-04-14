d37bb24422ff207217ff4b3154b291ac22af8ca8
Boiketlo20 <nzimandeyolanda058@gmail.com>
2025-04-14 04:36:11 +0200

    Shape Activity

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        Square square = new Square("Yellow", 6);
+        Rectangle rectangle = new Rectangle("Pink", 6, 3);
+        Circle circle = new Circle("Purple", 6);
+
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(square);
+        shapes.Add(rectangle);
+        shapes.Add(circle);
+
+        foreach (Shape shape in shapes)
+        {
+            string color = shape.GetColor(); 
+            double area = shape.GetArea();
+            Console.WriteLine($"{color}: {area}cm");
+        }
     }
 }
